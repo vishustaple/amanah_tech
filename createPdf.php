9d108c44051dd036f91275fb3c7628137b1fdab0
stapleom <omprakash@staplelogic.in>
2025-04-29 15:39:00 +0530

    new mobile issues

--- createPdf.php
+++ createPdf.php
@@ -1,22 +1,36 @@
 <?php
 if (isset($_FILES['pdf'])) {
     $pdf = $_FILES['pdf'];
-    // Define the target directory and file name
     $targetDir = "uploads/";  // Change this to your desired directory
-    $targetFile = $targetDir . time() . basename($pdf['name']);  // Use the original filename
-    
-    // Check if the file is a valid PDF
-    if ($pdf['type'] == 'application/pdf') {
-        // Move the uploaded file to the target directory
+    $targetFile = $targetDir . time() . basename($pdf['name']);  // Use original filename
+
+    // Validate file type
+    if (mime_content_type($pdf['tmp_name']) === 'application/pdf') {
         if (move_uploaded_file($pdf['tmp_name'], $targetFile)) {
-            echo json_encode(['success' => true, 'message' => 'PDF saved successfully!','filePaths' => $targetFile]);
+            echo json_encode([
+                'success' => true,
+                'message' => 'PDF saved successfully!',
+                'type' =>mime_content_type($pdf['tmp_name']),
+                'filePaths' => $targetFile
+            ]);
         } else {
-            echo json_encode(['success' => false, 'message' => 'Failed to save PDF on the server.']);
+            echo json_encode([
+                'success' => false,
+                'type' =>mime_content_type($pdf['tmp_name']),
+                'message' => 'Failed to save PDF on the server.'
+            ]);
         }
     } else {
-        echo json_encode(['success' => false, 'message' => 'Invalid file type. Only PDF files are allowed.']);
+        echo json_encode([
+            'success' => false,
+            'type' =>mime_content_type($pdf['tmp_name']),
+            'message' => 'Invalid file type. Only PDF files are allowed.'
+        ]);
     }
 } else {
-    echo json_encode(['success' => false, 'message' => 'No file uploaded.']);
+    echo json_encode([
+        'success' => false,
+        'message' => 'No file uploaded.'
+    ]);
 }
-?> 
+?>
